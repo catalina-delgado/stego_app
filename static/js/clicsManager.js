028f6738e33062a5db0d4d9e86e38d4a64528cd7
catalina-delgado <catalina08delgado@gmail.com>
2025-01-26 22:22:27 -0500

    Event controls

--- static/js/clicsManager.js
+++ static/js/clicsManager.js
@@ -1,59 +1,103 @@
-document.getElementById('botonDescargar').addEventListener('click', function() {
-    // Contenido del archivo a descargar (puedes cambiar esto por el contenido real de tu archivo)
-    var contenidoArchivo = 'Este es el contenido del archivo a descargar';
+document.addEventListener('DOMContentLoaded', function() {
+    // document.getElementById('downloadBoton').addEventListener('click', function() {
+    //     // Contenido del archivo a descargar (puedes cambiar esto por el contenido real de tu archivo)
+    //     var contenidoArchivo = 'Este es el contenido del archivo a descargar';
 
-    // Crear un objeto Blob que contiene el contenido del archivo
-    var blob = new Blob([contenidoArchivo], { type: 'text/plain' });
+    //     // Crear un objeto Blob que contiene el contenido del archivo
+    //     var blob = new Blob([contenidoArchivo], { type: 'text/plain' });
 
-    // Crear un objeto URL que apunta al Blob
-    var url = window.URL.createObjectURL(blob);
+    //     // Crear un objeto URL que apunta al Blob
+    //     var url = window.URL.createObjectURL(blob);
 
-    // Crear un enlace <a> para la descarga
-    var enlaceDescarga = document.createElement('a');
-    enlaceDescarga.href = url;
-    enlaceDescarga.download = 'archivo.txt'; // Nombre del archivo a descargar
-    enlaceDescarga.textContent = 'Descargar Archivo';
+    //     // Crear un enlace <a> para la descarga
+    //     var enlaceDescarga = document.createElement('a');
+    //     enlaceDescarga.href = url;
+    //     enlaceDescarga.download = 'archivo.txt'; // Nombre del archivo a descargar
+    //     enlaceDescarga.textContent = 'Descargar Archivo';
 
-    // Agregar el enlace al documento y simular un clic en él para iniciar la descarga
-    document.body.appendChild(enlaceDescarga);
-    enlaceDescarga.click();
+    //     // Agregar el enlace al documento y simular un clic en él para iniciar la descarga
+    //     document.body.appendChild(enlaceDescarga);
+    //     enlaceDescarga.click();
 
-    // Limpiar el objeto URL creado para liberar recursos
-    window.URL.revokeObjectURL(url);
-});
+    //     // Limpiar el objeto URL creado para liberar recursos
+    //     window.URL.revokeObjectURL(url);
+    // });
 
-document.getElementById('inputArchivo').addEventListener('change', function(evento) {
-    var archivo = evento.target.files[0];
-    
-    if (archivo) {
-        var infoArchivo = document.getElementById('infoArchivo');
-        var lector = new FileReader();
-
-        var icono = obtenerIcono(archivo.type);
-        var iconoElemento = document.createElement('img');
-        iconoElemento.src = icono;
-        infoArchivo.appendChild(iconoElemento);
-
-        var nombreElemento = document.createElement('span');
-        nombreElemento.textContent = archivo.name;
-        infoArchivo.appendChild(nombreElemento);
+    document.getElementById('uploadBoton').addEventListener('change', function(event) {
+        var archivo = evento.target.files[0];
         
-        lector.onload = function(eventoLector) {
-            var contenido = eventoLector.target.result;
+        if (archivo && archivo.type.match('image.*')) {
+            var lector = new FileReader();
+            lector.onload = function(eventoLector) {
+                var contenido = eventoLector.target.result;
+                
+                // Crear un elemento de imagen y establecer su fuente al contenido del archivo
+                var img = document.createElement('img');
+                img.src = contenido;
+                img.alt = 'Loaded image';
+                
+                // Agregar la imagen al div con id 'your_image'
+                var divImagen = document.getElementById('your_image');
+                divImagen.innerHTML = ''; // Limpiar cualquier contenido previo
+                divImagen.appendChild(img);
+            };
             
-            // Aquí puedes procesar el contenido del archivo utilizando SheetJS o cualquier otra biblioteca
-            console.log(contenido);
-        };
+            lector.readAsDataURL(archivo);
+        } else {
+            console.log('No se seleccionó ningún archivo de imagen.');
+        }
+    });
+
+    document.getElementById('exampleImageBoton').addEventListener('click', function(event) {
+        var divExampleImages = document.getElementById('example_images');
+        var parentDiv = divExampleImages.parentElement;
+        if (parentDiv.style.display === 'none' || parentDiv.style.display === '') {
+            parentDiv.style.display = 'block';
+        } else {
+            parentDiv.style.display = 'none';
+        }
         
-        lector.readAsBinaryString(archivo);
-    } else {
-        console.log('No se seleccionó ningún archivo.');
-    }
-});
+        var exampleImages = [
+            'static/img/cover image.png',
+            'static/img/stego image.png',
+            'static/img/985.png'
+        ];
+
+        var divExampleImages = document.getElementById('example_images');
+        divExampleImages.innerHTML = '';
+        
+        exampleImages.forEach(function(src) {
+            var imgContainer = document.createElement('div');
+                imgContainer.classList.add('example-image-container');
+                
+            var img = document.createElement('img');
+            img.src = src;
+            img.alt = 'Example image';
+            img.classList.add('img-fluid', 'mb-2');
+            img.addEventListener('click', function() {
+                var divImagen = document.getElementById('your_image');
+                divImagen.innerHTML = ''; 
+                var imgCargada = document.createElement('img');
+                imgCargada.src = src;
+                imgCargada.alt = 'Loaded image';
+                parentDiv.style.display = 'none';
+                divImagen.appendChild(imgCargada);
+            });
+
+            var label = document.createElement('div');
+            label.classList.add('image-label');
+            label.textContent = src.split('/').pop().replace(/\.[^/.]+$/, "");
+            
+            imgContainer.appendChild(img);
+            imgContainer.appendChild(label);
+            divExampleImages.appendChild(imgContainer);
+        });
+    });
+})
 
 function obtenerIcono(tipoArchivo) {
 
-    var extPermitidas = /(.xlsx|.xlx|.svg)$/i;
+    var extPermitidas = /(.jpg|.png|.pgm)$/i;
 
     if (tipoArchivo in extPermitidas) {
         tipo = 'iconxlsx'
